c6e39327037514ec7add9f2f9687281d2b9698c0
Aditya Mishra <aditya.mishra@molex.com>
2025-08-06 15:40:12 +0530

    committed to main

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import _map from 'lodash/map';
+import _filter from 'lodash/filter';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h3>Original Numbers: {{ numbers }}</h3>
+    <h3>Even Numbers (using _filter): {{ evenNumbers }}</h3>
+    <h3>Squares (using _map): {{ squares }}</h3>
+  `
+})
+export class AppComponent {
+  numbers = [1, 2, 3, 4, 5, 6];
+  evenNumbers = _filter(this.numbers, n => n % 2 === 0);
+  squares = _map(this.numbers, n => n * n);
+}
